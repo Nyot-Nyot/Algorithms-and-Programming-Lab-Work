7ce359337023bf141125e51b16013235f5f77614
NyotNyot <99807420+Nyot-Nyot@users.noreply.github.com>
2023-11-28 23:39:37 +0700

    Add files via upload

---
+++ Module 1 - Introduction to Algorithms and Programming/HelloWorld.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // baris ini menyertakan file header standar input/output
+int main() // baris ini mendeklarasikan fungsi utama program
+{
+    printf("Hello World!\n"); // baris ini mencetak teks "Hello World!" dan karakter baris baru ke layar
+    return 0; // baris ini mengembalikan nilai 0 sebagai status keluaran program
+}
