cc7deb1e6468c97b7f74666c4bcc8af407513cb4
NyotNyot <99807420+Nyot-Nyot@users.noreply.github.com>
2023-12-13 16:05:54 +0700

    Update jobsheet 01

--- Module 01 - Introduction to Algorithms and Programming/Code/HelloWorld.c
+++ Module 01 - Introduction to Algorithms and Programming/Code/HelloWorld.c
@@ -1,6 +1,14 @@
+/*
+    Nama: Dzaki Sultan Rabbani
+    NIM: 23343035
+    Prodi: Informatika
+*/
+
 #include <stdio.h> // baris ini menyertakan file header standar input/output
+
 int main() // baris ini mendeklarasikan fungsi utama program
 {
-    printf("Hello World!\n"); // baris ini mencetak teks "Hello World!" dan karakter baris baru ke layar
+    printf("Hello World!\n\n\n\n"); // baris ini mencetak teks "Hello World!" dan karakter baris baru ke layar
+    printf("By Dzaki\n");
     return 0; // baris ini mengembalikan nilai 0 sebagai status keluaran program
 }
