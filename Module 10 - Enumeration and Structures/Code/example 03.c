b04877cba2ed4965125804fe04730af8836125bd
NyotNyot <99807420+Nyot-Nyot@users.noreply.github.com>
2023-12-14 20:26:47 +0700

    update jobsheet 10

---
+++ Module 10 - Enumeration and Structures/Code/example 03.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+//membuat struct
+struct Mahasiswa {
+    char *name;
+    char *address;
+    int age;
+};
+
+void main(){
+    //menggunakan struct
+    struct Mahasiswa mhs1, mhs2;
+
+    //mengisi nilai ke struct
+    mhs1.name = "Dzaki";
+    mhs1.address = "Padang";
+    mhs1.age = 19;
+
+    mhs2.name = "Bambang";
+    mhs2.address = "Padang";
+    mhs2.age = 25;
+
+    //mencetak isi struct
+    printf("## Mahasiswa 1 ##\n");
+    printf("Nama : %s\n", mhs1.name);
+    printf("Alamat : %s\n", mhs1.address);
+    printf("Umur : %d\n\n", mhs1.age);
+    
+    printf("## Mahasiswa 2 ##\n");
+    printf("Nama : %s\n", mhs2.name);
+    printf("Alamat: %s\n", mhs2.address);
+    printf("Usia : %d\n", mhs2.age);
+}
