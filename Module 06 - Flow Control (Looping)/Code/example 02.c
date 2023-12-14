565e0979e91230245cc53ad89e8d9c7bf4e0d67c
NyotNyot <99807420+Nyot-Nyot@users.noreply.github.com>
2023-12-14 10:59:59 +0700

    update jobsheet 06

---
+++ Module 06 - Flow Control (Looping)/Code/example 02.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main(){
+    char ulangi;
+    int counter = 0;
+
+    //perulangan while
+    while(ulangi == 'y' || ulangi == 'Y'){
+        printf("Apakah kamu mau mengulang?\n");
+        printf("Jawab (y/t): ");
+        scanf(" %c", &ulangi);
+
+        //increment counter
+        counter++;
+    }
+
+    printf("\n\n-------------------------\n");
+    printf("Perulangan selesai!\n");
+    printf("Anda telah mengulang %d kali.\n", counter);
+}
---
+++ Module 06 - Flow Control (Looping)/Code/example 03.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main(){
+    char ulangi = 'y';
+    int counter = 0;
+
+    do{
+        printf("Apakah kamu mau mengulang?\n");
+        printf("jawab (y/t): ");
+        scanf(" %c", &ulangi);
+
+        //increment counter
+        counter++;
+    }while(ulangi == 'y');
+
+    printf("\n\n-------------------------\n");
+    printf("Perulangan selesai!\n");
+    printf("Anda telah mengulang %d kali.\n", counter);
+}
