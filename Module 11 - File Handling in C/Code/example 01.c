79ab231506b9ee2de4c438e9e7ed04922195693b
NyotNyot <99807420+Nyot-Nyot@users.noreply.github.com>
2023-12-14 21:09:55 +0700

    update jobshyet 11

---
+++ Module 11 - File Handling in C/Code/example 01.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main(){
+    char buff[255];
+    char text[255];
+    FILE *fptr;
+
+    //membuka file untuk ditulis
+    fptr = fopen("puisi.txt", "w");
+
+    //mengambil input dari user
+    printf("Inputkan isi file: ");
+    fgets(text, sizeof(text), stdin);
+
+    //menulis ke text file
+    fputs(text, fptr);
+
+    printf("File berhasil ditulis\n");
+
+    //tutup file setelah ditulis
+    fclose(fptr);
+}
